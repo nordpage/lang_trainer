9b925a76a429122c4675cffeb5897fc690be6903
aurilaus <aurilaus@gmail.com>
2024-01-07 22:26:53 +0200

    Updates

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './header.module.css'
 import wolf from '../../images/wolf.png'
+import { TiThMenu } from "react-icons/ti";
+
 function Header() {
     return (
         <div className={styles.header}>
@@ -8,7 +10,7 @@ function Header() {
                 <img alt="Logo" className={styles.logo} src={wolf}/>
             </div>
             <div className={styles.title}>Trainer for Lithuanian cases</div>
-            <div>123</div>
+            <div><TiThMenu /></div>
         </div>
     );
 }
